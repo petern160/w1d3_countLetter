0f24e54184e24f0bbfc032f2cc26c8a864ac974b
petern160 <peternguyen160@gmail.com>
2019-04-03 16:08:24 +0000

    added so it does not count the spaces in the string

--- countLetter.js
+++ countLetter.js
@@ -1,15 +1,20 @@
+
 function countLetter (str){
 	var count = {};
-	for (var i = 0; i < str.length; i++){
-		var letter = str.charAt(i)
-		if(count[letter]){
-		  count[letter]++
-		} else {
-		  count[letter] = 1
-		}
+  var trimmed = str.trim().split(' ');
+  console.log(str)
+	for (var i = 0; i < trimmed.length; i++){
+    for(var j = 0; j < trimmed[i].length; j++) {
+      var letter = trimmed[i][j]
+      if(count[letter]){
+        count[letter]++
+      } else {
+        count[letter] = 1
+      }
+    }
 	}
 
 	return count;
 }
 
-console.log(countLetter("apple"))
+console.log(countLetter("     lighthouse in the house "))
