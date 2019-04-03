43f526b3e6a3797795e9f2853806ef11d1d529ef
petern160 <peternguyen160@gmail.com>
2019-04-03 00:22:17 +0000

    added letter increment

--- countLetter.js
+++ countLetter.js
@@ -1,6 +1,15 @@
-function countLetter = (str){
-	var count = {}
+function countLetter (str){
+	var count = {};
 	for (var i = 0; i < str.length; i++){
-		
+		var letter = str.charAt(i)
+		if(count[letter]){
+		  count[letter]++
+		} else {
+		  count[letter] = 1
+		}
 	}
+
+	return count;
 }
+
+console.log(countLetter("apple"))
